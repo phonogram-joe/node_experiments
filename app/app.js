eca79ed4dac3f4be24dd928775f135f66b0f2ac8
Joe Savona <joe@phonogram.co.jp>
2012-02-03 19:07:40 +0900

    app: use stylus, add route & view to list projects (note: db connection is failing so cannot confirm this works)

--- app/app.js
+++ app/app.js
@@ -1,26 +1,45 @@
-
-/**
- * Module dependencies.
- */
-
+// Dependencies
 var express = require('express')
 	, routes = require('./routes')
+	, stylus = require('stylus')
+	, stylusCompile
+	, db = require('./models/db')
+	, app;
 
-var app = module.exports = express.createServer();
+app = module.exports = express.createServer();
+
+stylusCompile = function(str, path) {
+	return stylus(str)
+		//.import(__dirname + '/css/mixins/blueprint')
+		.set('filename', path)
+		.set('warn', true)
+		.set('linenos', true);
+}
 
 // Configuration
-
 app.configure(function(){
+	app.use(express.logger({
+		format: ':date :method :url :status :response-time'
+	}));	
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
+	app.use(stylus.middleware({
+		linenos: true,
+		compress: true,
+		src: __dirname + '/public',
+		compile: stylusCompile
+	}));
 	app.use(app.router);
 	app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	app.use(express.errorHandler({
+		dumpExceptions: true, 
+		showStack: true
+	})); 
 });
 
 app.configure('production', function(){
@@ -28,8 +47,8 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', routes.index);
+app.get('/projects', routes.projects);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,23 @@
-
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+	res.render('index', { title: 'Express' })
+};
+
+/*
+ * GET projects list
+ */
+exports.projects = function(req, res, next) {
+	var Project = require('../models/project')
+		, timeoutId;
+	timeoutId = setTimeout(function() {
+		next(new Error('timeout on db query'));
+	}, 500);
+	Project.findAll().success(function(projects) {
+		clearTimeout(timeoutId);
+		res.render('projects', {
+			projects: projects
+		});
+	});
 };
